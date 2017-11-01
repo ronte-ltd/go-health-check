76d1dbdf377fbe9b0ed1009210eda69f24d954be
Alexandr Kozlenkov <sah4ez32@gmail.com>
2017-11-01 15:28:21 +0300

    Fixed test for memcached.

--- checkers/MemcachedChecker_test.go
+++ checkers/MemcachedChecker_test.go
@@ -7,6 +7,7 @@ import (
 
 func IgnoreNewMemcachedChecker(t *testing.T) {
 	mc, err := memcache.New("127.0.0.1:11211")
+	defer mc.Close()
 	if err != nil {
 		t.Fatalf("Cannot connect to server: %s", err.Error())
 	}
